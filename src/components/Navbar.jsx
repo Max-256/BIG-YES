3380e62ffa321bd3d05a828d6b8bbb4e43837386
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-02-21 08:16:08 +0300

    build the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {BiSearch, BiShoppingBag} from 'react-icons/bi';
+import {FaStoreAlt} from 'react-icons/fa'; 
 
-const Navbar = () => {
-    return (
-        <div>
-            <h1>Navbar</h1> 
-        </div>
-    );
-};
+function NavBar() {
 
-export default Navbar;
+  return (    
+      <Navbar bg="light" variant="light" className='navbar'>
+
+        <Container>
+          <Nav className="navItems">
+
+            <NavLink to="/market" className="nav-link">
+              <li className='navIcon'><BiShoppingBag/></li>
+              <li>Market</li>
+            </NavLink>
+
+            <NavLink to="/search" className="nav-link searchNav">
+              <li className='navIcon'><BiSearch/></li>
+              <li>Search</li>            
+            </NavLink>
+
+            <NavLink to="/myShop" className="nav-link">
+              <li className='navIcon'><FaStoreAlt/></li>
+              <li>My Shop</li> 
+            </NavLink>
+
+          </Nav>
+        </Container>
+
+      </Navbar>
+  );
+}
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
-
