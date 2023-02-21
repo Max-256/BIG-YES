e8c4fd53da1445c714f39399fda3823aa3d2e819
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-02-21 17:51:54 +0300

    build the search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 
 const Search = () => {
+    const [query, setQuery] = useState("");
+
+    const handleChange = e => setQuery(e.target.value);
+
+    const handleSearch = searchQuery => {
+        console.log(searchQuery);
+        setQuery("");
+    }
+
     return (
         <div className='search'>
-            <h1>Search</h1>
+            <h1>Wan Duc Stores</h1>
+            <div className='input-group'>
+
+              <input  value={query}
+                      placeholder="enter item"
+                      onChange={handleChange} />
+
+              <button className='search-button'
+                      onClick={() => handleSearch(query)}>Search
+              </button>
+            </div>
         </div>
     );
 };
