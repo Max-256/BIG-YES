bc9ff3fef80a4afabb0d1c581f0f3172ad02ff4c
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-03-03 12:49:06 +0300

    unwrap goodscontainer

--- src/components/common/GoodsContainer.jsx
+++ src/components/common/GoodsContainer.jsx
@@ -4,13 +4,15 @@ const GoodsContainer = ({goods}) => {
     return (
         <div className='market grid grid-1x2 grid-1x3 grid-1x4'>
             {goods.map(good =>
-            <Link key={good._id} to={`/good-details/${good._id}`} className='card m-2'>
+            <div key={good._id}>
+            <Link  to={`/good-details/${good._id}`} className='card m-2'>
                <img src={good.image} alt="" />
                <div className='card-body'>
                <span className='good-title'>{good.good}</span>
                <span className='badge bg-primary'>UGX.{good.price}</span> 
                </div>                
             </Link>
+            </div>
             )}
         </div>
     );
