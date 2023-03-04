130ea119d8eb32beb28f9db6bb5198e2c71b0112
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-03-04 22:53:35 +0300

    fix bug

--- src/components/common/GoodsContainer.jsx
+++ src/components/common/GoodsContainer.jsx
@@ -5,21 +5,13 @@ import Spinner from './Spinner';
 import { useState } from 'react';
 
 const GoodsContainer = ({goods}) => {
-    const [loading, setLoading] = useState(true);
-
-    async function  fetchImage(url) {
-        const {data} = await axios.get(url);
-        setLoading(false);
-        return data;
-    }
-
     return (
         <div className='market grid grid-1x2 grid-1x3 grid-1x4'>
             {goods.map(good =>                  
                 <Link key={good._id} to={`/good-details/${good._id}`}  className='card m-2'>
 
                  {loading && <Spinner />}
-                 {!loading && <img src={fetchImage(good.image)} alt="" />}      
+                 {!loading && <img src={good.image} alt="" />}      
                             
                 <div className='card-body'>                 
                   <span className='good-title'>{good.good}</span>
