6a14f17e3c982a03a7d17c3a2caf770db6c23b07
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-03-04 22:42:47 +0300

    add image spinner

--- src/components/common/GoodsContainer.jsx
+++ src/components/common/GoodsContainer.jsx
@@ -1,13 +1,25 @@
 import { Link } from 'react-router-dom';
 import { formatPrice } from '../../services/marketService';
+import axios from 'axios';
+import Spinner from './Spinner';
+import { useState } from 'react';
 
 const GoodsContainer = ({goods}) => {
+    const [loading, setLoading] = useState(true);
+
+    async function  fetchImage(url) {
+        const {data} = await axios.get(url);
+        setLoading(false);
+        return data;
+    }
 
     return (
         <div className='market grid grid-1x2 grid-1x3 grid-1x4'>
             {goods.map(good =>                  
                 <Link key={good._id} to={`/good-details/${good._id}`}  className='card m-2'>
-                <img src={good.image} alt="" />      
+
+                 {loading && <Spinner />}
+                 {!loading && <img src={fetchImage(good.image)} alt="" />}      
                             
                 <div className='card-body'>                 
                   <span className='good-title'>{good.good}</span>
